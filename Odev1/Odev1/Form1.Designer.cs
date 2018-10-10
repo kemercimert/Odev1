3765eeb84034ce4677e6f172b0c37b7afc15d190
kemercimert <>
2018-10-10 21:10:18 +0300

    1.Odev
    1.Odev

---
+++ Odev1/Odev1/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Odev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.dosyaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kaydetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grileştirmeYöntemleriToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ortalamaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.bT709ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lumaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.açıklıkYöntemiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kanalÇıkarımıToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dosyaToolStripMenuItem,
+            this.grileştirmeYöntemleriToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1003, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dosyaToolStripMenuItem
+            // 
+            this.dosyaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.açToolStripMenuItem,
+            this.kaydetToolStripMenuItem});
+            this.dosyaToolStripMenuItem.Name = "dosyaToolStripMenuItem";
+            this.dosyaToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dosyaToolStripMenuItem.Text = "Dosya";
+            // 
+            // açToolStripMenuItem
+            // 
+            this.açToolStripMenuItem.Name = "açToolStripMenuItem";
+            this.açToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.açToolStripMenuItem.Text = "Aç";
+            this.açToolStripMenuItem.Click += new System.EventHandler(this.açToolStripMenuItem_Click);
+            // 
+            // kaydetToolStripMenuItem
+            // 
+            this.kaydetToolStripMenuItem.Name = "kaydetToolStripMenuItem";
+            this.kaydetToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kaydetToolStripMenuItem.Text = "Kaydet";
+            this.kaydetToolStripMenuItem.Click += new System.EventHandler(this.kaydetToolStripMenuItem_Click);
+            // 
+            // grileştirmeYöntemleriToolStripMenuItem
+            // 
+            this.grileştirmeYöntemleriToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ortalamaToolStripMenuItem,
+            this.bT709ToolStripMenuItem,
+            this.lumaToolStripMenuItem,
+            this.açıklıkYöntemiToolStripMenuItem,
+            this.kanalÇıkarımıToolStripMenuItem});
+            this.grileştirmeYöntemleriToolStripMenuItem.Name = "grileştirmeYöntemleriToolStripMenuItem";
+            this.grileştirmeYöntemleriToolStripMenuItem.Size = new System.Drawing.Size(136, 20);
+            this.grileştirmeYöntemleriToolStripMenuItem.Text = "Grileştirme Yöntemleri";
+            // 
+            // ortalamaToolStripMenuItem
+            // 
+            this.ortalamaToolStripMenuItem.Name = "ortalamaToolStripMenuItem";
+            this.ortalamaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ortalamaToolStripMenuItem.Text = "Ortalama";
+            this.ortalamaToolStripMenuItem.Click += new System.EventHandler(this.ortalamaToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(480, 460);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(498, 43);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(480, 460);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // bT709ToolStripMenuItem
+            // 
+            this.bT709ToolStripMenuItem.Name = "bT709ToolStripMenuItem";
+            this.bT709ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.bT709ToolStripMenuItem.Text = "BT-709";
+            this.bT709ToolStripMenuItem.Click += new System.EventHandler(this.bT709ToolStripMenuItem_Click);
+            // 
+            // lumaToolStripMenuItem
+            // 
+            this.lumaToolStripMenuItem.Name = "lumaToolStripMenuItem";
+            this.lumaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.lumaToolStripMenuItem.Text = "Luma";
+            this.lumaToolStripMenuItem.Click += new System.EventHandler(this.lumaToolStripMenuItem_Click);
+            // 
+            // açıklıkYöntemiToolStripMenuItem
+            // 
+            this.açıklıkYöntemiToolStripMenuItem.Name = "açıklıkYöntemiToolStripMenuItem";
+            this.açıklıkYöntemiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.açıklıkYöntemiToolStripMenuItem.Text = "Açıklık Yöntemi";
+            this.açıklıkYöntemiToolStripMenuItem.Click += new System.EventHandler(this.açıklıkYöntemiToolStripMenuItem_Click);
+            // 
+            // kanalÇıkarımıToolStripMenuItem
+            // 
+            this.kanalÇıkarımıToolStripMenuItem.Name = "kanalÇıkarımıToolStripMenuItem";
+            this.kanalÇıkarımıToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.kanalÇıkarımıToolStripMenuItem.Text = "Kanal Çıkarımı";
+            this.kanalÇıkarımıToolStripMenuItem.Click += new System.EventHandler(this.kanalÇıkarımıToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 528);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dosyaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kaydetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grileştirmeYöntemleriToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ortalamaToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ToolStripMenuItem bT709ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lumaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem açıklıkYöntemiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem kanalÇıkarımıToolStripMenuItem;
+    }
+}
+
---
+++ Odev1/Odev1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev1
+{
+    public partial class Form1 : Form
+    {
+        Bitmap kaynak, islem;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ortalamaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = (renkliRenk.R + renkliRenk.G + renkliRenk.B) / 3;
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+
+            pictureBox2.Image = islem;
+        }
+
+        private void kaydetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "PNG|*.png";
+            DialogResult sonuc = saveFileDialog1.ShowDialog();
+            ImageFormat format = ImageFormat.Png;
+            if (sonuc == DialogResult.OK)
+            {
+                islem.Save(saveFileDialog1.FileName, format);
+            }
+        }
+
+        private void bT709ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = ((renkliRenk.R * 2125 / 10000) + (renkliRenk.G * 7154 / 10000) + (renkliRenk.B * 72 / 1000));
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+            pictureBox2.Image = islem;
+        }
+
+        private void lumaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = ((renkliRenk.R * 3 / 10) + (renkliRenk.G * 59 / 100) + (renkliRenk.B * 11 / 100));
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+            pictureBox2.Image = islem;
+        }
+
+        private void açıklıkYöntemiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gri;
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+
+                    int a = 0;
+
+                    if (renkliRenk.R >= renkliRenk.G)
+                        a = renkliRenk.R;
+                    if (renkliRenk.R >= renkliRenk.B)
+                        a = renkliRenk.R;
+                    if (renkliRenk.G >= renkliRenk.R)
+                        a = renkliRenk.G;
+                    if (renkliRenk.G >= renkliRenk.B)
+                        a = renkliRenk.G;
+                    if (renkliRenk.B >= renkliRenk.R)
+                        a = renkliRenk.B;
+                    if (renkliRenk.B >= renkliRenk.G)
+                        a = renkliRenk.B;
+
+                    int b = 0;
+
+                    if (renkliRenk.R <= renkliRenk.G)
+                        b = renkliRenk.R;
+                    if (renkliRenk.R <= renkliRenk.B)
+                        b = renkliRenk.R;
+                    if (renkliRenk.G <= renkliRenk.R)
+                        b = renkliRenk.G;
+                    if (renkliRenk.G <= renkliRenk.B)
+                        b = renkliRenk.G;
+                    if (renkliRenk.B <= renkliRenk.R)
+                        b = renkliRenk.B;
+                    if (renkliRenk.B <= renkliRenk.G)
+                        b = renkliRenk.B;
+
+                    gri = (a + b) / 2;
+
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+
+                }
+            }
+            pictureBox2.Image = islem;
+        }
+
+        private void kanalÇıkarımıToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int gen = kaynak.Width;
+            int yuk = kaynak.Height;
+
+            islem = new Bitmap(gen, yuk);
+
+            for (int y = 0; y < yuk; y++)
+            {
+                for (int x = 0; x < gen; x++)
+                {
+
+                    Color renkliRenk = kaynak.GetPixel(x, y);
+                    int gri = (renkliRenk.R | renkliRenk.G | renkliRenk.B);
+                    Color griRenk = Color.FromArgb(gri, gri, gri);
+                    islem.SetPixel(x, y, griRenk);
+                }
+            }
+            pictureBox2.Image = islem;
+        }
+
+        private void açToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult sonuc = openFileDialog1.ShowDialog();
+            if (sonuc == DialogResult.OK)
+            {
+                kaynak = new Bitmap(openFileDialog1.FileName);
+                pictureBox1.Image = kaynak;
+            }
+        }
+    }
+}
